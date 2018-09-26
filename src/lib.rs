ce9b3b858c9db2599b68d7a599cd54bc83ba920e
Josh Hejna <josh.hejna@gmail.com>
2018-09-26 00:34:12 -0700

    Genericize Everything

--- src/lib.rs
+++ src/lib.rs
@@ -6,37 +6,50 @@ use alga::linear::VectorSpace;
 pub fn rk4<S, V>(tn: S, xn: V, vn: V, h: S, acc_fun: impl Fn(S, V) -> V) -> (V, V)
 where
     S: Real,
-    V: VectorSpace,
+    V: VectorSpace<Field = S> + Copy,
 {
+    let _2 = S::from_i32(2).unwrap();
+    let _6 = S::from_i32(6).unwrap();
+
+    let __2 = _2.recip();
+    let __6 = _6.recip();
+
     let k1 = acc_fun(tn, vn);
-    let k2 = acc_fun(tn + h / 2., vn + k1 * h / 2.);
-    let k3 = acc_fun(tn + h / 2., vn + k2 * h / 2.);
-    let k4 = acc_fun(tn + h, vn + k3 * h);
-    let vn1 = vn + (k1 + 2. * k2 + 2. * k3 + k4) * (h / 6.);
+    let k2 = acc_fun(tn + (h * __2), vn + k1 * (h * __2));
+    let k3 = acc_fun(tn + (h * __2), vn + k2 * (h * __2));
+    let k4 = acc_fun(tn + h, vn + (k3 * h));
+    let vn1 = vn + (k1 + k2 * _2 + k3 * _2 + k4) * (h * __6);
 
     let k1x = vn;
-    let k2x = vn + k1x * h / 2.;
-    let k3x = vn + k2x * h / 2.;
+    let k2x = vn + k1x * (h / _2);
+    let k3x = vn + k2x * (h / _2);
     let k4x = vn + k3x * h;
-    let xn1 = xn + (k1x + 2. * k2x + 2. * k3x + k4x) * (h / 6.);
+    let xn1 = xn + (k1x + k2x * _2 + k3x * _2 + k4x) * (h * __6);
 
     return (xn1, vn1);
 }
 
 #[cfg(test)]
 mod tests {
+    extern crate nalgebra;
+    use self::nalgebra::core::MatrixMN;
+    use self::nalgebra::U1;
     use super::*;
+
     #[test]
     fn it_works() {
+        pub type V = MatrixMN<f64, U1, U1>;
         let h = 0.01;
-        let acc_fun = |_t, v| 9.81 - 1.0 * v;
-        let mut t = vec![0.];
-        let mut x = vec![0.];
-        let mut v = vec![0.];
+
+        let g = V::from([9.81]);
+        let acc_fun = |_t, v| g - 1.0 * v;
+        let mut t = Vec::new();
+        let mut x = Vec::new();
+        let mut v = Vec::new();
 
         let mut tn = 0.;
-        let mut xn = 0.;
-        let mut vn = 0.;
+        let mut xn = V::zeros_generic(U1, U1);
+        let mut vn = V::zeros_generic(U1, U1);
 
         for _ in 1..10000 {
             let a = rk4(tn, xn, vn, h, acc_fun);
